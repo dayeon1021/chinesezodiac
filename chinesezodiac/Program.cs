d89ecbc78e2dceb03810d04c735265752573c44b
dayeon1021 <s2018s20@e-mirim.hs.kr>
2020-05-06 21:54:24 +0900

    2020.05.06 실습보고서 문제 5번

--- chinesezodiac/Program.cs
+++ chinesezodiac/Program.cs
@@ -10,6 +10,56 @@ namespace chinesezodiac
     {
         static void Main(string[] args)
         {
+            string[] yearName = { "원숭이", "닭", "개", "돼지", "쥐", "소", "호랑이", "토끼", "용", "뱀", "말", "양" };
+
+            Console.Write("태어난 년도를 입력하세요... : ");
+            int year = int.Parse(Console.ReadLine());
+
+            switch (year % 12)
+            {
+                case 0:
+                    Console.WriteLine(yearName[year % 12]+"띠 입니다!");
+                    break;
+                case 1:
+                    Console.WriteLine(yearName[year % 12] + "띠 입니다!");
+                    break;
+                case 2:
+                    Console.WriteLine(yearName[year % 12] + "띠 입니다!");
+                    break;
+                case 3:
+                    Console.WriteLine(yearName[year % 12] + "띠 입니다!");
+                    break;
+                case 4:
+                    Console.WriteLine(yearName[year % 12] + "띠 입니다!");
+                    break;
+                case 5:
+                    Console.WriteLine(yearName[year % 12] + "띠 입니다!");
+                    break;
+                case 6:
+                    Console.WriteLine(yearName[year % 12] + "띠 입니다!");
+                    break;
+                case 7:
+                    Console.WriteLine(yearName[year % 12] + "띠 입니다!");
+                    break;
+                case 8:
+                    Console.WriteLine(yearName[year % 12] + "띠 입니다!");
+                    break;
+                case 9:
+                    Console.WriteLine(yearName[year % 12] + "띠 입니다!");
+                    break;
+                case 10:
+                    Console.WriteLine(yearName[year % 12] + "띠 입니다!");
+                    break;
+                case 11:
+                    Console.WriteLine(yearName[year % 12] + "띠 입니다!");
+                    break;
+                case 12:
+                    Console.WriteLine(yearName[year % 12] + "띠 입니다!");
+                    break;
+                default:
+                    Console.WriteLine("잘못 입력하셨습니다.");
+                    break;
+            }
         }
     }
 }
